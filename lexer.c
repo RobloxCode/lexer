233dec8c05082ba8f216fb1695360fbda11e446a
Miguel Loranca <mloranca703@gmail.com>
2026-05-10 17:27:19 -0600

    added the code to scan through the code sample, i did it by calculating the length of the whole string and then just iterating over it, at first i was going to use a while loop, but i had an error where the last statement of the string woulnd't be added to the cur_word_buff, but at the end just used a for loop since i had already calculated the length of the code sample

--- lexer.c
+++ lexer.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "TokenArr.h"
+
+#define WORD_MAX_CAP 255
 
 char *KEYWORDS[] = {
     // control flow
@@ -19,13 +23,26 @@ char *KEYWORDS[] = {
     "struct", "union", "enum"
 };
 
+void clear_buff(char *buff);
 TokenArr *lexeme(char *code_sample);
 
 int main(void)
 {
-    char *code_example = "int x =  10 + 5;";
+    char *code_example = "int x = 10 + 5; ";
+    TokenArr *token_arr = lexeme(code_example);
+    if (!token_arr)
+        return EXIT_FAILURE;
 
-    return 0;
+    TokenArr_println(token_arr);
+    TokenArr_deinit(&token_arr);
+
+    return EXIT_SUCCESS;
+}
+
+void clear_buff(char *buff)
+{
+    for (size_t i = 0; buff[i] != '\0'; ++i)
+        buff[i] = 0;
 }
 
 TokenArr *lexeme(char *code_sample)
@@ -33,6 +50,41 @@ TokenArr *lexeme(char *code_sample)
     if (!code_sample)
         return NULL;
 
+    size_t code_sample_len = strlen(code_sample);
+    TokenArr *token_arr = TokenArr_init(code_sample_len);
+    if (!token_arr)
+        return NULL;
 
+    char cur_word_buff[WORD_MAX_CAP] = {0};
+    size_t cur_word_buff_pos = 0;
+
+    for (size_t i = 0; i < code_sample_len; ++i) {
+        char cur_char = code_sample[i];
+
+        if (cur_char == ' ') {
+            puts(cur_word_buff);
+            clear_buff(cur_word_buff);
+            cur_word_buff_pos = 0;
+            continue;
+        }
+
+        cur_word_buff[cur_word_buff_pos++] = cur_char;
+    }
+
+    // size_t code_cur_pos = 0;
+    // while (code_sample[code_cur_pos] != '\0') {
+    //     if (code_sample[code_cur_pos] == ' ') {
+    //         puts(cur_word_buff);
+    //
+    //         clear_buff(cur_word_buff);
+    //         cur_word_buff_pos = 0;
+    //         // code_cur_pos++;
+    //     }
+    //
+    //     char cur_char = code_sample[code_cur_pos++];
+    //     cur_word_buff[cur_word_buff_pos++] = cur_char;
+    // }
+
+    return token_arr;
 }
 
