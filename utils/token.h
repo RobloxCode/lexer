125f25332d35de5e611f98cce4807208917ed380
Miguel Loranca <mloranca703@gmail.com>
2026-06-10 11:18:39 -0600

    removed the TokenType enum

--- utils/token.h
+++ utils/token.h
@@ -4,18 +4,6 @@
 #define TOKEN_VALUE_MAX_CAP 255
 #define TOKEN_TYPE_MAX_CAP  255
 
-typedef enum {
-    TOKEN_IDENTIFIER,
-    TOKEN_KEYWORD,
-    TOKEN_NUMBER,
-    TOKEN_OPERATOR,
-    TOKEN_DELIMETER,
-    TOKEN_STRING,
-    TOKEN_CHARACTER,
-    TOKEN_COMMENT,
-    TOKEN_ENDOFFILE,
-} TokenType;
-
 typedef struct {
     char type[TOKEN_TYPE_MAX_CAP];
     char value[TOKEN_VALUE_MAX_CAP];
