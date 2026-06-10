13cb80dcbc52a8894ed864d0b8a3ab09ac9561f6
Miguel Loranca <mloranca703@gmail.com>
2026-06-10 12:13:20 -0600

    added more token types

--- utils/token.c
+++ utils/token.c
@@ -3,13 +3,55 @@
 #include <stdio.h>
 #include <string.h>
 
-char *operators[] = {
-    "+",  "-",  "*",  "/",  "%",  "=",  "+=", "-=", "*=", "/=", "%=",
-    "&=", "|=", "~=", "^=", "<<", ">>", "&",  "|",  "~",  "^",  "&&",
-    "||", "!=", "==", "<",  ">",  "<=", ">=", "++", "--",
+#define ARRLEN(a) (sizeof a / sizeof a[0])
+
+typedef struct {
+    char *val;
+    char *tok_type_str;
+    TokType tok_type;
+} Exp;
+
+Exp exp_operators[] = {
+    {"+", "PLUS", TOK_PLUS},
+    {"-", "SUBSTRACT", TOK_SUBSBTRACT},
+    {"*", "MULTIPLICATION", TOK_MULTIPLICATION},
+    {"/", "DIVISION", TOK_DIVISION},
+    {"%", "MODULE", TOK_MODULE},
+    {"=", "ASSIGN", TOK_ASSIGN},
+    {"+=", "ADD ASSIGNMENT", TOK_ADD_ASSIGNMENT},
+    {"-=", "SUB ASSIGNMENT", TOK_SUB_ASSIGNMENT},
+    {"*=", "MUL ASSIGNMENT", TOK_MUL_ASSIGNMENT},
+    {"/=", "DIV ASSIGNMENT", TOK_DIV_ASSIGNMENT},
+    {"%=", "MOD ASSIGNMENT", TOK_MOD_ASSIGNMENT},
+    {"&=", "AND ASSIGNMENT", TOK_BITWISE_AND_ASSIGNMENT},
+    {"|=", "OR ASSIGNMENT", TOK_BITWISE_OR_ASSIGNMENT},
+    {"~=", "NOT ASSIGNMENT", TOK_BITWISE_NOT_ASSIGNMENT},
+    {"^=", "XOR ASSIGNMENT", TOK_BITWISE_XOR_ASSIGNMENT},
+    {"<<", "LBITSHIFT", TOK_LBITSHIFT},
+    {">>", "RBITSHIFT", TOK_RBITSHIFT},
+    {"&", "BITWISE AND", TOK_BITWISE_AND},
+    {"|", "BITWISE OR", TOK_BITWISE_OR},
+    {"~", "BITWISE NOT", TOK_BITWISE_NOT},
+    {"^", "BITWISE XOR", TOK_BITWISE_XOR},
+    {"&&", "AND", TOK_AND},
+    {"||", "OR", TOK_OR},
+    {"!=", "NOT EQUAL", TOK_NOT_EQ},
+    {"==", "EQUAL", TOK_EQ},
+    {"<", "LESS", TOK_LESS},
+    {">", "GREATER", TOK_GREATER},
+    {"<=", "LESS_EQ", TOK_LESS_EQ},
+    {">=", "GREATER_EQ", TOK_GREATER_EQ},
+    {"++", "INCREMENT", TOK_INCREMENT},
+    {"--", "DECREMENT", TOK_DECREMENT},
 };
 
-char *delimeters[] = {"(", ")", "{", "}", "[", "]", ",", ".", ";", ":"};
+Exp exp_delimeters[] = {
+    {"(", "LPAR", TOK_LPAR},   {")", "RPAR", TOK_RPAR},
+    {"{", "LCURL", TOK_LCURL}, {"}", "RCURL", TOK_RCURL},
+    {"[", "LBRAC", TOK_LBRAC}, {"]", "RBRAC", TOK_RBRAC},
+    {",", "COMMA", TOK_COMMA}, {".", "DOT", TOK_DOT},
+    {":", "COLON", TOK_COLON}, {";", "SEMICOLON", TOK_SEMICOLON},
+};
 
 char *keywords[] = {"if",       "else",    "switch", "case",     "default",
                     "for",      "while",   "do",     "break",    "continue",
@@ -32,7 +74,7 @@ static int _is_hash(const char *s) {
 }
 
 static int _is_keyword(const char *s) {
-    for (size_t i = 0; i < sizeof keywords / sizeof keywords[0]; ++i) {
+    for (size_t i = 0; i < ARRLEN(keywords); ++i) {
         if (strcmp(s, keywords[i]) == 0) {
             return 1;
         }
@@ -42,8 +84,8 @@ static int _is_keyword(const char *s) {
 }
 
 static int _is_operator(const char *s) {
-    for (size_t i = 0; i < sizeof operators / sizeof operators[0]; ++i) {
-        if (strcmp(s, operators[i]) == 0) {
+    for (size_t i = 0; i < ARRLEN(exp_operators); ++i) {
+        if (strcmp(s, exp_operators[i].val) == 0) {
             return 1;
         }
     }
@@ -52,8 +94,8 @@ static int _is_operator(const char *s) {
 }
 
 static int _is_delimeter(const char *s) {
-    for (size_t i = 0; i < sizeof delimeters / sizeof delimeters[0]; ++i) {
-        if (strcmp(s, delimeters[i]) == 0) {
+    for (size_t i = 0; i < ARRLEN(exp_delimeters); ++i) {
+        if (strcmp(s, exp_delimeters[i].val) == 0) {
             return 1;
         }
     }
@@ -141,7 +183,12 @@ void token_init(Token *t, const char *word) {
     }
 
     else if (_is_operator(word)) {
-        strcpy(t->type, "OPERATOR");
+        for (size_t i = 0; i < ARRLEN(exp_operators); ++i) {
+            if (strcmp(word, exp_operators[i].val) == 0) {
+                strcpy(t->type, exp_operators[i].tok_type_str);
+                break;
+            }
+        }
         strcpy(t->value, word);
     }
 
@@ -151,7 +198,12 @@ void token_init(Token *t, const char *word) {
     }
 
     else if (_is_delimeter(word)) {
-        strcpy(t->type, "DELIMETER");
+        for (size_t i = 0; i < ARRLEN(exp_delimeters); ++i) {
+            if (strcmp(word, exp_delimeters[i].val) == 0) {
+                strcpy(t->type, exp_delimeters[i].tok_type_str);
+                break;
+            }
+        }
         strcpy(t->value, word);
     }
 
--- utils/token.h
+++ utils/token.h
@@ -4,14 +4,57 @@
 #define TOKEN_VALUE_MAX_CAP 255
 #define TOKEN_TYPE_MAX_CAP  255
 
+typedef enum {
+    // delimeters
+    TOK_LPAR,
+    TOK_RPAR,
+    TOK_LCURL,
+    TOK_RCURL,
+    TOK_LBRAC,
+    TOK_RBRAC,
+    TOK_COMMA,
+    TOK_DOT,
+    TOK_COLON,
+    TOK_SEMICOLON,
+
+    // operators
+    TOK_PLUS,
+    TOK_SUBSBTRACT,
+    TOK_MULTIPLICATION,
+    TOK_DIVISION,
+    TOK_MODULE,
+    TOK_ASSIGN,
+    TOK_ADD_ASSIGNMENT,
+    TOK_SUB_ASSIGNMENT,
+    TOK_MUL_ASSIGNMENT,
+    TOK_DIV_ASSIGNMENT,
+    TOK_MOD_ASSIGNMENT,
+    TOK_BITWISE_AND_ASSIGNMENT,
+    TOK_BITWISE_OR_ASSIGNMENT,
+    TOK_BITWISE_NOT_ASSIGNMENT,
+    TOK_BITWISE_XOR_ASSIGNMENT,
+    TOK_LBITSHIFT,
+    TOK_RBITSHIFT,
+    TOK_BITWISE_AND,
+    TOK_BITWISE_OR,
+    TOK_BITWISE_NOT,
+    TOK_BITWISE_XOR,
+    TOK_AND,
+    TOK_OR,
+    TOK_NOT_EQ,
+    TOK_EQ,
+    TOK_LESS,
+    TOK_GREATER,
+    TOK_LESS_EQ,
+    TOK_GREATER_EQ,
+    TOK_INCREMENT,
+    TOK_DECREMENT,
+} TokType;
+
 typedef struct {
     char type[TOKEN_TYPE_MAX_CAP];
     char value[TOKEN_VALUE_MAX_CAP];
 } Token;
-
-extern char *operators[];
-extern char *delimeters[];
-extern char *keywords[];
 
 void token_init(Token *t, const char *word);
 void token_print(Token t);
