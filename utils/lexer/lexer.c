d3ff9eaf0711159e6aea4a749045c842dd43d3cb
Miguel Loranca <mloranca703@gmail.com>
2026-07-23 22:44:13 -0600

    got rid off ahead_word strbuf

--- utils/lexer/lexer.c
+++ utils/lexer/lexer.c
@@ -37,7 +37,7 @@ static Lexer *lexer_init(const char *path) {
     l->line = 0;
     l->col = 0;
     strbuf_init(&l->cur_word);
-    strbuf_init(&l->ahead_word);
+    memset(l->peek_buf, 0, sizeof l->peek_buf);
 
     return l;
 }
@@ -107,13 +107,13 @@ Lexer *lexeme(char *path) {
         }
 
         strbuf_push(&lexer->cur_word, (char)lexer->cur_char);
-        strbuf_set(&lexer->ahead_word, (char)lexer->peek_char, 0);
+        lexer->peek_buf[0] = (char)lexer->peek_char;
 
         size_t found_idx_buf = 0;
         size_t found_idx_buf_two = 0;
 
         if (is_operator(lexer->cur_word.items, &found_idx_buf)
-            && is_operator(lexer->ahead_word.items, &found_idx_buf_two)) {
+            && is_operator(lexer->peek_buf, &found_idx_buf_two)) {
             // TODO: the ahead_char gets tokenized by itself even though
             // theres the continue to skip the iteration
 
@@ -149,7 +149,7 @@ Lexer *lexeme(char *path) {
             continue;
         }
 
-        if (is_sintax_element(lexer->ahead_word.items)) {
+        if (is_sintax_element(lexer->peek_buf)) {
             token_init(&token, &lexer->cur_word, lexer->line, lexer->col);
             emit_token(lexer, &token);
             strbuf_clear(&lexer->cur_word);
