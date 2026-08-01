0860e5b85132c298989690609565082656723ed0
Miguel Loranca <mloranca703@gmail.com>
2026-07-31 22:53:30 -0600

    fixed a bug when handling numbers

--- utils/lexer/lexer_scan.c
+++ utils/lexer/lexer_scan.c
@@ -21,31 +21,19 @@ void handle_str(Lexer *l) {
 }
 
 int handle_number(Lexer *l) {
-    int ret = 0;
     int count_dot = 0;
 
-    int digits_count = 0;
+    while (is_digit(l->peek_char) || l->peek_char == '.') {
+        advance(l);
 
-    advance(l);
-    while (is_digit((char)l->cur_char) || l->cur_char == '.') {
         if (l->cur_char == '.') {
             count_dot++;
         }
 
         strbuf_push(&l->cur_word, (char)l->cur_char);
-        advance(l);
-        digits_count++;
     }
 
-    l->col += digits_count;
-
-    if (count_dot > 1) {
-        ret = 1;
-    } else {
-        ret = 0;
-    }
-
-    return ret;
+    return count_dot > 1;
 }
 
 void handle_one_line_comment(Lexer *l) {
