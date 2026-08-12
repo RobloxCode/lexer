1bcf051bfec85d1ea1f7198da32b263fd316cb92
Miguel Loranca <mloranca703@gmail.com>
2026-08-11 19:07:08 -0600

    added an assert macro

--- tests/lex_test/test.c
+++ tests/lex_test/test.c
@@ -3,12 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ASSERT_MSG(cond, fmt, ...)                                             \
+    do {                                                                       \
+        if (!(cond)) {                                                         \
+            fprintf(stderr,                                                    \
+                    "Assertion failed: (%s), file %s, line %d\n" fmt "\n",     \
+                    #cond, __FILE__, __LINE__, ##__VA_ARGS__);                 \
+            abort();                                                           \
+        }                                                                      \
+    } while (0)
+
 int main(void) {
-    Lexer *l = lexer_lex("");
-    if (!l) {
-        fprintf(stderr, "Failed to tokenize: %s\n", "");
-        return EXIT_FAILURE;
-    }
+    const char *path = "";
+    Lexer *l = lexer_lex(path);
+
+    ASSERT_MSG(l != NULL, "Failed to tokenize %s", path);
+
     lexer_deinit(&l);
     return 0;
 }
