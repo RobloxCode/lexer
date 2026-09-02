5147122114e6cffbd386aa42912d3ac6d573e65f
Miguel Loranca <mloranca703@gmail.com>
2026-09-01 21:56:02 -0600

    removed an unnessesary enum

--- utils/token_def/token_def.h
+++ utils/token_def/token_def.h
@@ -14,44 +14,6 @@ typedef struct {
     TokType tok_type; /* < Actuall token type*/
 } TokenDef;
 
-/**
- * @brief indexes of the operators in the exp_operators array
- */
-typedef enum {
-    PLUS_IDX,
-    MINUS_IDX,
-    STAR_IDX,
-    SLASH_IDX,
-    MODULO_IDX,
-    ASSIGN_IDX,
-    ADD_ASSIGN_IDX,
-    SUB_ASSIGN_IDX,
-    MUL_ASSIGN_IDX,
-    DIV_ASSIGN_IDX,
-    MOD_ASSIGN_IDX,
-    AND_ASSIGN_IDX,
-    OR_ASSIGN_IDX,
-    XOR_ASSIGN_IDX,
-    LBITSHIFT_IDX,
-    RBITSHIFT_IDX,
-    BIT_AND_IDX,
-    BIT_OR_IDX,
-    BIT_NOT_IDX,
-    BIT_XOR_IDX,
-    AND_IDX,
-    OR_IDX,
-    NOT_EQUAL_IDX,
-    EQUAL_IDX,
-    LESS_IDX,
-    GREATER_IDX,
-    LESS_EQ_IDX,
-    GREATER_EQ_IDX,
-    INCREMENT_IDX,
-    DECREMENT_IDX,
-
-    EXP_OPERATORS_COUNT
-} token_definitions_idxs;
-
 extern const TokenDef tok_definitions[];
 extern const size_t tok_definitions_len;
 
