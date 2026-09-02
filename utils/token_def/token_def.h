06e8e7811e3bb01d1087d6342e4597b7292e2cc5
Miguel Loranca <mloranca703@gmail.com>
2026-09-01 22:01:15 -0600

    added docs

--- utils/token_def/token_def.h
+++ utils/token_def/token_def.h
@@ -6,15 +6,25 @@
 #include <stddef.h>
 
 /**
- * @brief Individual expression from the C language
+ * @brief Struct containing the lexeme value of a token and its display value to
+ *        help when printing to the console, also, every value is mapped to a
+ *        token type
  */
 typedef struct {
-    char *display_name;
-    char *lexeme;     /* < The elements that the expression has */
+    char *display_name; /* < word that will go alongside when printing
+                           (INCREMENT, LPAREN, ...) */
+    char *lexeme;     /* < The elements that the expression has (++, ), ...) */
     TokType tok_type; /* < Actuall token type*/
 } TokenDef;
 
+/**
+ * Array containing a struct TokenDef for every TokType
+ */
 extern const TokenDef tok_definitions[];
+
+/**
+ * length of the array tok_definitions
+ */
 extern const size_t tok_definitions_len;
 
 #endif
