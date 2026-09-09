5721ef015175a46065d8ba68bd0310addc5f2f0f
Miguel Loranca <mloranca703@gmail.com>
2026-09-08 21:43:19 -0600

    added a test

--- src/source.c
+++ src/source.c
@@ -1,15 +1,35 @@
-#include <stdio.h>
-int main() {
-    int x = 42;
-    float y = 3.14f;
-    char *msg = "hello, world";
-    if (x >= 10 && y != 0.0) {
-        x++;
+// single-line comment: should be skipped entirely, no token
+/* multi-line
+   comment: should also be skipped entirely, no token */
+
+int main(void) {
+    int count = 0;
+    float average = 3.14;
+    char *name = "hello world";
+
+    int valid_int = 42;
+    float valid_float = 3.14159;
+    float suffixed = 10.5f;
+    float invalid_num = 1.2.3;
+
+    count = count + 1;
+    count = count - 1;
+    count = count * 2;
+    count = count / 2;
+
+    if (count == 0) {
+        count += 1;
     }
-    return x;
-    double integer = 2 >= 8 ? 3 : 22;
-    int invalid = 3.3.1;
-    if (1 <- 3) {
-        return 1;
+
+    if (count != 0 && average <= 3.14) {
+        count -= 1;
     }
+
+    if (count >= 0 || average >= 0) {
+        count++;
+    }
+
+    int arr[3] = {1, 2, 3};
+
+    return 0;
 }
