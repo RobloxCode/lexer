66d814828081a7dcd6e10e7209269513a1293d27
Miguel Loranca <mloranca703@gmail.com>
2026-09-11 23:20:15 -0600

    created a file for source.c

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DEFAULT_PATH "src/source.c"
+#define DEFAULT_PATH "examples/source.c"
 
 int main(int argc, char **argv) {
     const char *path = DEFAULT_PATH;
