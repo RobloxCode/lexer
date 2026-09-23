d33890826ec80121a46cbddb49f8b1b546ad4611
RobloxCode <167715447+RobloxCode@users.noreply.github.com>
2026-09-22 21:28:41 -0600

    Delete build directory

--- build/token/token.d
+++
@@ -1,5 +0,0 @@
-build/token/token.o: src/token/token.c include/lexer/token/token.h \
- include/lexer/str_buf/str_buf.h include/lexer/token_def/token_def.h
-include/lexer/token/token.h:
-include/lexer/str_buf/str_buf.h:
-include/lexer/token_def/token_def.h:
